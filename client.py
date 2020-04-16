aedcbe22338f96e3b8205895e2869f97f19e60f3
Aditya Kadole <60019192+Aditya-Kadole@users.noreply.github.com>
2020-04-16 19:17:32 -0400

    Update client.py

--- client.py
+++ client.py
@@ -13,23 +13,32 @@ hostname = socket.gethostbyname(socket.gethostname())
 print(hostname)
 portnumber = 10000
 
-
 # Create a socket object
 #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)		  
 #s.connect((Host_RS,Port_RS )) 
 
-a = None
-while True:
-  print("1. Registering with RS")
-  print("2. Requesting the Peer List")
-  print("3. Leave the network")
-  a = int(input("Enter your request: "))
-  if a == 1:
-    s1 = socket.socket()
-    s1.connect((Host_RS,Port_RS))
+def registration():
     x = 'Register_to_RS'
     message = x + ' <cr> <lf>\nHostname ' + hostname + ' <cr> <lf>\nPortnumber ' + str(portnumber) + ' <cr> <lf>\n'
-    s1.send(message.encode())
+    s.send(message.encode())
     received_message = s1.recv(2048).decode()
     print(received_message)
+    s.close()
+
+def requesting_peerlist():
+    
+
+
+    
+choice = None
+while True:
+    print("1. Registering with RS\n 2. Requesting the Active Peer List\n 3. Leave the network")
+    choice = int(input("Enter your request: "))
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((Host_RS,Port_RS))
+    if choice == 1:
+        registration()
+    elif choice == 2:
+        requesting_peerlist()
+      
 

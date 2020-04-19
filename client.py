e419318f173ec840269cd5db5b7fa83e94d5deef
Aditya Kadole <60019192+Aditya-Kadole@users.noreply.github.com>
2020-04-19 16:52:51 -0400

    Update client.py

--- client.py
+++ client.py
@@ -34,7 +34,24 @@ def requesting_peerlist():
     s.send(message.encode())
     received_message = s.recv(2048).decode()
     print(received_message)
-    recv_msg = received_message[received_message.index('\n')+4:]
+    recv_message = received_message.split('\n')
+    #print(type(recv_message), len(recv_message), recv_message)
+    if len(recv_message) == 1:
+        print("No peers active")
+    else:
+        new_msg = recv_message[1]
+        b = new_msg.strip('][').strip('}{').split(':')
+        port_id = b[2]
+        print(port_id)
+        #print(type(b))
+        ip_value = b[1].strip('"').split(',')
+        ip_value = ip_value[0]
+        #new_ip = ip_value[0]
+        #print(recv_message)
+        #print("IP Value is")
+        #print(type(ip_value))
+        #print(ip_value)
+        #recv_msg = {key: received_message[key] for key in received_message.keys()& {'ip'}}
     s.close()
 
 def leave_message():

d701f63d1966108d60b8cffabecdd4c592c79533
Niranjan Pandeshwar <45427747+Niranjanpandeshwar@users.noreply.github.com>
2020-04-30 17:12:34 -0400

    Update registrationserver.py

--- registrationserver.py
+++ registrationserver.py
@@ -1,10 +1,11 @@
+# Import the required packages
 import socket
 import time
 import json
 import datetime
 
-
 class peers:
+    # Contructor
     def __init__(self, hostname, port):
         self.hostname = hostname
         self.port = port
@@ -12,16 +13,17 @@ class peers:
         self.flag = False
         self.ip = None
 
-    
+    # Initial state of the peer
     def initial(self,IP):
         self.ip = IP
         self.flag = True
     
-
+#Create  linkedlist with peer information
 class peer_linked_list:
     def __init__(self):
         self.head = peers(None, None)
 
+    # function to check if the peer is already in the network, if yes, update the IP, else add the peer to the network
     def upd(self, host, port, IP):
         new_node = peers(host,port)
         current_node = self.head
@@ -37,7 +39,7 @@ class peer_linked_list:
         current_node.next.flag = True
         return
 
-    # prepare a list of active peers
+    # function to prepare a list of active peers
     def send_AP(self, IP):
         current_node = self.head
         dictn = {}
@@ -51,7 +53,7 @@ class peer_linked_list:
             # If the peer being checked is still active and 
             # the IP is not same as the IP of the peer who requests the info then 
             # add the details to the dictionary
-            print(current_node.flag, current_node.ip, current_node.hostname, current_node.port)
+            print(current_node.flag, current_node.ip, current_node.hostname, current_node.port, IP)
             if (current_node.flag == True and current_node.ip != IP):
                 dictn = {
                     'ip': current_node.ip,
@@ -60,6 +62,7 @@ class peer_linked_list:
                 list_d.append(dictn.copy())
         return list_d
 
+    # Function to leave the network
     def leave(self, host, port):
         current_node = self.head
         while current_node.next!= None:
@@ -68,9 +71,11 @@ class peer_linked_list:
                 current_node.flag=False
 
 
-    
+# Create an object   
 Peerlist = peer_linked_list()
-server_port = 23472
+# Set port number for RS
+server_port = 23473
+#Create a TCP socket for RS
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('', server_port))
 
@@ -79,38 +84,27 @@ server_socket.listen(5)
 print("Registration server ready")
 
 while True:
-    print("Reached here")
+    #print("Reached here")
     conn, address = server_socket.accept()
-    #print("conn=", conn)
     server_IP = address[0]
-    #print("server_IP=", server_IP)
-    #print("address", address)
+    print(server_IP)
     request_format = conn.recv(2048).decode()
     req = json.loads(request_format)
     print('#############################REQUEST RECEIVED #############################')
     print(req)
     # Extract the host part from the request
-    #Hostname = req[req.index('Hostname') + 9:req.index(' <cr> <lf>\nPortnumber')]
-    Message_type = req["msg"]
-    Hostname = req["Hostname"]
-    Portnumber = req["Portnumber"]
-    
-    # Extract the port number from the request
-    #Portnumber = req[req.index('Portnumber') + 11:req.index(' <cr> <lf>\n')]
-    # Extract cookie information from the request
-    #Cookie_id = req[req.index('Cookie_id') + 10:request.index(' <cr> <lf>')]
+    Message_type, Hostname, Portnumber = req["msg"], req["Hostname"], req["Portnumber"]
     
     # Check the message sent by peer and decide which function of RS needs to be executed
 
     # 1- peer wants to register and get into the network.
-    #if 'Register_to_RS' in req:
     if Message_type == 'Register_to_RS':
         Peerlist.upd(Hostname, Portnumber, server_IP)
-        #response = '200 OK' + '<cr> <lf>\nFrom:' + socket.gethostname() + '<cr><lf>\nDate:' + str(datetime.datetime.now()) + '<cr> <lf>'
         response = json.dumps({"msg" : '200 OK'})
         conn.send(response.encode())
         conn.close()
 
+    # 2- peer wants to know the list of active peers.
     elif Message_type == 'Active_peer':
         l_active = Peerlist.send_AP(server_IP)
         print(l_active)
@@ -119,23 +113,20 @@ while True:
         print("query data=", query_data)
 
         # Check if any peer is active or not
-
         if len(l_active) > 0:
             list_active = '200 OK'+'\n' + query_data
-         
             conn.send(list_active.encode())
             conn.close()
+
+        # No peer is active
         else:
             list_active = '404 No Active Peer'
-        
             conn.send(list_active.encode())
             conn.close()
 
+    # 3- peer wants to leave the network.
     elif Message_type == 'Leave_the_server':
         Peerlist.leave(Hostname, Portnumber)
-        Leave_message = '200 OK' + '<cr> <lf>\nFrom:' + socket.gethostname() + '<cr><lf>\nDate:' + str(datetime.datetime.now()) + '<cr> <lf>\n<cr> <lf>\n' + 'Left successfully'
-    
+        Leave_message = 'Left successfully' + '\nDate:' + str(datetime.datetime.now())
         conn.send(Leave_message.encode())
         conn.close()
-
-
